af825f0a063c16b0b0db16ac7f78809bdecbf7cf
0xD3adC0d3 <vincent.jerome@protonmail.com>
2017-11-01 11:43:59 +0000

    First commit

---
+++ README.md
@@ -0,0 +1,4 @@
+This is the template I use in most of my scripts.
+This repository is only used because the structure of my code is almost always the same.
+
+"Being smart is being lazy"
---
+++ deadTemplate.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+
+'''
+
+'''
+
+import argparse
+import logging
+import sys
+
+API_KEY = "CENSORED"
+
+parser = argparse.ArgumentParser(description='Description')
+
+parser.add_argument('-v', '--verbose', action='store_true', help='Enable verbose output', default=False)
+
+if len(sys.argv) < 3:
+    parser.print_help()
+    exit(1)
+
+args = parser.parse_args()
+
+level = logging.DEBUG if args.verbose else logging.INFO
+logging.basicConfig(level=level, format='[%(asctime)s.%(msecs)03d]  %(message)s', datefmt="%Y-%m-%d %H:%M:%S")
